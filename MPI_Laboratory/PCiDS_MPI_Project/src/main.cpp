98d3a88f65b79ebde73b75cd22d69321e4cf87e5
dylsonowski <michal.dyla@o2.pl>
2022-06-08 10:29:52 +0200

    PCiDS MPI Project

--- MPI_Laboratory/PCiDS_MPI_Project/src/main.cpp
+++ MPI_Laboratory/PCiDS_MPI_Project/src/main.cpp
@@ -1,3 +1,4 @@
+#pragma warning(disable : 4996)
 #include "mpi.h"
 #include <iostream>
 #include <cassert>
@@ -8,14 +9,8 @@
 #include <iterator>
 
 int main(int argc, char* argv[]) {
-	std::string inputFile = "data/infile1500_1200.bin";
+	std::string inputFile = "../../PCiDS_MPI_Project/data/infile1500_1200.bin";
 	const unsigned int inputXDimention = 1500, inputYDimention = 1200;
-
-	std::ifstream inputData(inputFile, std::ios::in | std::ifstream::binary);
-	assert(inputData.good());
-
-	std::vector<int> data((std::istreambuf_iterator<char>(inputData)), std::istreambuf_iterator<char>());
-
 	int rank, size;
 	MPI_Init(&argc, &argv);
 
@@ -23,6 +18,22 @@ int main(int argc, char* argv[]) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	std::cout << "Hello from process " << rank << " out of " << size << std::endl;
+
+	if (rank == 0) {
+		std::ifstream inputData(inputFile, std::ios::in | std::ifstream::binary);
+		assert(inputData.good());
+
+		std::vector<uint8_t> data((std::istreambuf_iterator<char>(inputData)), std::istreambuf_iterator<char>());
+		inputData.close();
+		std::cout << static_cast<unsigned int>(*std::max_element(data.begin(), data.end())) << std::endl;
+
+		while (!data.empty()) {
+			MPI_Bcast(&data, data.size(), MPI_UINT8_T, rank, MPI_COMM_WORLD);
+		}
+	}
+	else {
+
+	}
 
 	MPI_Finalize();
 
